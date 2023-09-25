aae2aa5e8efb921b2f17004df20e02142d3061e4
Mohamed Essam <77198018+MohamedEssam-900009@users.noreply.github.com>
2023-09-25 21:58:21 +0300

    Create auth cubit

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -1,5 +1,8 @@
+import 'package:dalel/core/services/service_locator.dart';
+import 'package:dalel/features/auth/presentation/auth_cubit/cubit/auth_cubit.dart';
 import 'package:dalel/features/auth/presentation/views/sign_up_view.dart';
 import 'package:dalel/features/on_boarding/presentation/views/on_boarding_view.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
 import '../../features/auth/presentation/views/sign_in_view.dart';
@@ -17,11 +20,17 @@ final GoRouter router = GoRouter(
     ),
     GoRoute(
       path: '/signUp',
-      builder: (context, state) => const SignUpView(),
+      builder: (context, state) => BlocProvider(
+        create: (context) => getIt<AuthCubit>(),
+        child: const SignUpView(),
+      ),
     ),
     GoRoute(
       path: '/signIn',
-      builder: (context, state) => const SignInView(),
+      builder: (context, state) => BlocProvider(
+        create: (context) => getIt<AuthCubit>(),
+        child: const SignInView(),
+      ),
     ),
   ],
 );
